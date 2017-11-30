2866e561b675390007a5c49167e54f0450c60846
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2017-11-30 13:53:21 +0300

    Add Get Average Rate methods

--- src/Lykke.Service.IcoExRate/Controllers/RatesController.cs
+++ src/Lykke.Service.IcoExRate/Controllers/RatesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Lykke.Service.IcoExRate.Core.Domain;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Lykke.Service.IcoExRate.Controllers
 {
@@ -44,6 +45,14 @@ namespace Lykke.Service.IcoExRate.Controllers
             return Ok(rates);
         }
 
+        [HttpGet("{pair}/{dateTimeUtc}/average")]
+        [ProducesResponseType(typeof(AverageRateResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetAverageRateByPairAndDateTime([Required] Pair pair, [Required] DateTime dateTimeUtc)
+        {
+            return Ok(await GetAverageResponse(pair, dateTimeUtc));
+        }
+
         [HttpGet("{pair}/latest")]
         [ProducesResponseType(typeof(RateResponse[]), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
@@ -59,6 +68,14 @@ namespace Lykke.Service.IcoExRate.Controllers
             };
 
             return Ok(rates);
+        }
+
+        [HttpGet("{pair}/latest/average")]
+        [ProducesResponseType(typeof(AverageRateResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetLatestAverageRateByPairAndDateTime([Required] Pair pair)
+        {
+            return Ok(await GetAverageResponse(pair, DateTime.UtcNow));
         }
 
         [HttpGet("latest")]
@@ -81,13 +98,75 @@ namespace Lykke.Service.IcoExRate.Controllers
             return Ok(rates);
         }
 
+        [HttpGet("latest/average")]
+        [ProducesResponseType(typeof(AverageRateResponse[]), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetLatestAverageRateByPairAndDateTime()
+        {
+            var now = DateTime.UtcNow;
+
+            var averages = new AverageRateResponse[]
+            {
+                await GetAverageResponse(Pair.BTCUSD, now),
+                await GetAverageResponse(Pair.ETHUSD, now)
+            };
+
+            return Ok(averages);
+        }
+
+        private async Task<AverageRateResponse> GetAverageResponse(Pair pair, DateTime dateTimeUtc)
+        {
+            dateTimeUtc = dateTimeUtc.ToUniversalTime();
+
+            var rates = new RateResponse[]
+            {
+                await GetRateResponse(pair, Market.Lykke, dateTimeUtc),
+                await GetRateResponse(pair, Market.Kraken, dateTimeUtc),
+                await GetRateResponse(pair, Market.Bitfinex, dateTimeUtc)
+            };
+
+            // remove rates that do not have rate and are older by 10 minutes
+            var validRates = rates
+                .Where(f => f.Rate.HasValue && f.CreatedUtc.HasValue && dateTimeUtc.Subtract(f.CreatedUtc.Value).Minutes < 10)
+                .OrderBy(f => f.Rate)
+                .ToList();
+
+            if (!validRates.Any())
+            {
+                return null;
+            }
+
+            if (validRates.Count() >= 3)
+            {
+                // remove more distant rate
+                var firtDiff = validRates[1].Rate - validRates[0].Rate;
+                var lastDiff = validRates[validRates.Count() - 1].Rate - validRates[validRates.Count() - 2].Rate;
+
+                if (firtDiff > lastDiff)
+                {
+                    validRates.Remove(validRates.First());
+                }
+                else
+                {
+                    validRates.Remove(validRates.Last());
+                }
+            }
+
+            return new AverageRateResponse
+            {
+                AverageRate = validRates.Average(f => f.Rate),
+                Rates = rates
+            };
+        }
+
         private async Task<RateResponse> GetRateResponse(Pair pair, Market market, DateTime createdUtc)
         {
             var rate = await _exRateService.GetRate(pair, market, createdUtc);
 
             return new RateResponse
             {
-                Name = Enum.GetName(typeof(Market), market),
+                Market = Enum.GetName(typeof(Market), market),
+                Pair = Enum.GetName(typeof(Pair), pair),
                 Rate = rate?.ExchangeRate,
                 CreatedUtc = rate?.CreatedUtc
             };
--- src/Lykke.Service.IcoExRate/Models/RateResponse.cs
+++ src/Lykke.Service.IcoExRate/Models/RateResponse.cs
@@ -4,8 +4,15 @@ namespace Lykke.Service.IcoExRate.Models
 {
     public class RateResponse
     {
-        public string Name { get; set; }
+        public string Market { get; set; }
+        public string Pair { get; set; }
         public decimal? Rate { get; set; }
         public DateTime? CreatedUtc { get; set; }
+    }
+
+    public class AverageRateResponse
+    {
+        public decimal? AverageRate { get; set; }
+        public RateResponse[] Rates { get; set; }
     }
 }
